adfad8121451a105d253c80f31591d1f77a66bc9
Evgeny <evgeny@false.team>
2022-08-19 20:33:44 +0300

    Initial commit

---
+++ handlers/__init__.py
@@ -0,0 +1,12 @@
+# from . import module1, module2
+
+
+def register(client):
+    # module1.register(client)
+    # module2.register(client)
+    pass
+
+
+__all__ = [
+    'register'
+]
---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+import asyncio
+from os import getenv
+
+from telethon import TelegramClient
+
+import handlers
+
+
+async def main():
+    client = TelegramClient('vpn', int(getenv('API_ID')), getenv('API_HASH'))
+    client.parse_mode = 'markdown'
+    await client.start(bot_token=getenv('TOKEN'))
+    handlers.register(client)
+    print('Ready')
+    await client.run_until_disconnected()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
