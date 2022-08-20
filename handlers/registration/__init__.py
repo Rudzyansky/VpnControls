246625afd4dff7e4459b6601eb91777d9511d626
Evgeny <evgeny@false.team>
2022-08-20 11:52:58 +0300

    Reformat loading translations

--- handlers/registration/__init__.py
+++ handlers/registration/__init__.py
@@ -1,6 +1,4 @@
-from gettext import translation, NullTranslations
-
-from localization import languages, localedir
+from localization import load_translations
 from . import register_msg, invite_iq, accept_cq, decline_cq, revoke_cq, start_msg
 
 modules = [register_msg,
@@ -10,10 +8,7 @@ modules = [register_msg,
            revoke_cq,
            start_msg]
 
-translations: list[NullTranslations] = []
-
-for language in languages:
-    translations[language] = translation(__name__, localedir, [language], fallback=True)
+translations = load_translations(__name__)
 
 __all__ = [
     'modules',
--- localization.py
+++ localization.py
@@ -1,2 +1,8 @@
+from gettext import translation, NullTranslations
+
 localedir = 'lang'
 languages = ['en', 'ru']
+
+
+def load_translations(domain: str) -> dict[str:NullTranslations]:
+    return {i: translation(domain, localedir, [i], fallback=True) for i in languages}
