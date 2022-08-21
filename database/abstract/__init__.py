623153ecf0f2dc773aa3f8b73cf4a82065eed82b
Evgeny <evgeny@false.team>
2022-08-21 13:23:10 +0300

    Add transaction factory

--- database/abstract/__init__.py
+++ database/abstract/__init__.py
@@ -1,11 +1,35 @@
+from functools import wraps
+from typing import Type
+
 from .accounts import Accounts
 from .tokens import Tokens
 from .transaction import Transaction
 from .users import Users
+
+
+def transaction_factory(_type: Type[Transaction], *_args, **_kwargs):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            in_args = next(filter(lambda i: isinstance(i, Transaction), args), None) is not None
+            in_kwargs = hasattr(kwargs, 't')
+            if in_args or in_kwargs:
+                return func(*args, **kwargs)
+            t = _type(*_args, **_kwargs)
+            t.begin()
+            result = func(*args, **kwargs, t=t)
+            t.end()
+            return result
+
+        return wrapper
+
+    return decorator
+
 
 __all__ = [
     'Accounts',
     'Tokens',
     'Users',
     'Transaction',
+    'transaction_factory'
 ]
--- database/sqlite/__init__.py
+++ database/sqlite/__init__.py
@@ -2,9 +2,14 @@ from .accounts import AccountsSqlite
 from .tokens import TokensSqlite
 from .transaction import TransactionSqlite
 from .users import UsersSqlite
+from ..abstract import Transaction, transaction_factory
+
+transaction = transaction_factory(TransactionSqlite, 'clients.db')
+
 
 __all__ = [
     'AccountsSqlite',
     'TokensSqlite',
     'UsersSqlite',
+    'transaction'
 ]
