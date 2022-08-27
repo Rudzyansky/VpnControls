0c21561e3fa94ec5317bad3c676a0bd40b1237c0
Evgeny <evgeny@false.team>
2022-08-27 19:57:26 +0300

    Controls refactoring

--- controls/controlsabstract.py
+++ controls/controlsabstract.py
@@ -1,19 +1,34 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
+from entities.account import Account
 
 
 class Controls(ABC):
     @classmethod
     @abstractmethod
-    def add_user(cls, username: str): pass
+    def add_user(cls, user_id: int, username: str, password: str) -> Optional[Account]: pass
 
     @classmethod
     @abstractmethod
-    def remove_user(cls, username: str): pass
+    def remove_user(cls, user_id: int, id: int) -> int: pass
 
     @classmethod
     @abstractmethod
-    def reset_password(cls, username: str): pass
+    def set_password(cls, user_id: int, id: int, password: str) -> int: pass
 
     @classmethod
     @abstractmethod
-    def change_username(cls, username: str, new_username: str): pass
+    def set_username(cls, user_id: int, id: int, username: str) -> int: pass
+
+    @classmethod
+    @abstractmethod
+    def get_account(cls, user_id: int, id: int) -> Optional[Account]: pass
+
+    @classmethod
+    @abstractmethod
+    def get_accounts(cls, user_id: int, *ids: int) -> list[Account]: pass
+
+    @classmethod
+    @abstractmethod
+    def change_hook(cls): pass
--- controls/controlsstroke.py
+++ controls/controlsstroke.py
@@ -1,66 +1,115 @@
 import re
+import subprocess
 from re import Pattern
-from typing import AnyStr, IO
+from typing import AnyStr, IO, Optional
 
 from controls import utils
 from controls.controlsabstract import Controls
 from controls.utils import from_hex, to_hex
+from entities.account import Account
 
 
 class StrokeControls(Controls):
     line_pattern: Pattern[AnyStr]
     filepath: str
 
-    def __init__(self, filepath: str = '/etc/strongswan/ipsec.secrets'):
+    def __init__(self, filepath: str = '/etc/strongswan/users/ipsec.%s.secrets'):
         super().__init__()
         self.line_pattern = re.compile('"#(?P<username>[a-zA-Z0-9]+)" : EAP "0x(?P<password>[a-zA-Z0-9]+)"')
         self.filepath = filepath
 
     @classmethod
-    def add_user(cls, username: str):
+    def add_user(cls, user_id: int, username: str, password: str) -> Optional[Account]:
         password = utils.gen_password()
         line = f'"#{to_hex(username)}" : EAP "0x{to_hex(password)}"\n'
-        with open(cls.filepath, 'a') as f:
-            start, = next(find_by_username(f, username), (None,))
-            if start is not None:
-                return False
+        with open(cls.filepath % user_id, 'a') as f:
+            pos = f.tell()
             f.write(line)
+        return Account(username=username, password=password, position=pos)
 
     @classmethod
-    def remove_user(cls, username: str):
-        with open(cls.filepath, 'r+', encoding='utf-8') as f:
-            start, end, = next(find_by_username(f, username), (None, None,))
-            if start is not None and end is not None:
-                f.seek(end)
-                data = f.read()
-                f.seek(start)
-                f.write(data)
-                f.truncate()
+    def remove_user(cls, user_id: int, pos: int) -> int:
+        with open(cls.filepath % user_id, 'r+', encoding='utf-8') as f:
+            f.seek(pos)
+            f.readline()  # skip current line
+            start, end = pos, f.tell()
+            tmp = f.read()
+            f.seek(start)
+            f.write(tmp)
+            f.truncate()
+        return end - start
 
     @classmethod
-    def reset_password(cls, username: str):
-        password = to_hex(utils.gen_password())
-        with open(cls.filepath, 'r+', encoding='utf-8') as f:
-            start, end, _pw, index = next(find_by_username(f, username), (None, None, None, None))
-            if start is not None and end is not None:
-                f.seek(start + index + 2)
-                f.write(password)
+    def set_password(cls, user_id: int, pos: int, password: str) -> int:
+        with open(cls.filepath % user_id, 'r+', encoding='utf-8') as f:
+            start, end = get_password_pos(f, pos)
+            replace_by_indexes(f, start, end, to_hex(password))
+        return end - start
 
     @classmethod
-    def change_username(cls, username: str, new_username: str):
-        with open(cls.filepath, 'r+', encoding='utf-8') as f:
-            start, = next(find_by_username(f, new_username), (None,))
-            if start is not None:
-                return False
-            start, end, = next(find_by_username(f, username), (None, None))
-            if start is None:
-                return False
+    def set_username(cls, user_id: int, pos: int, username: str) -> int:
+        with open(cls.filepath % user_id, 'r+', encoding='utf-8') as f:
+            start, end = get_username_pos(f, pos)
+            replace_by_indexes(f, start, end, to_hex(username))
+        return end - start
+
+    @classmethod
+    def get_account(cls, user_id: int, pos: int) -> Optional[Account]:
+        with open(cls.filepath % user_id, 'r+', encoding='utf-8') as f:
+            f.seek(pos)
+            matches = cls.line_pattern.match(f.readline())
+        return Account(username=matches[0], password=matches[1], position=pos)
+
+    @classmethod
+    def get_accounts(cls, user_id: int, *positions: int) -> list[Account]:
+        result: list[Account] = list()
+        with open(cls.filepath % user_id, 'r+', encoding='utf-8') as f:
+            for pos in positions:
+                f.seek(pos)
+                matches = cls.line_pattern.match(f.readline())
+                result.append(Account(username=matches[0], password=matches[1], position=pos))
+        return result
+
+    @classmethod
+    def change_hook(cls):
+        subprocess.run(['strongswan', 'rereadsecrets'])
 
 
-def find_quoted_str(line: str, start: int = 0):
+def find_quoted_str(line: str, start: int = None):
     q1 = line.find('"', start)
     q2 = line.find('"', q1 + 1)
     return q1 + 1, q2 - 1
+
+
+def rfind_quoted_str(line: str, end: int = None):
+    q2 = line.rfind('"', 0, end)
+    q1 = line.rfind('"', 0, q2 - 1)
+    return q1 + 1, q2 - 1
+
+
+def get_username_pos(f: IO, pos: int):
+    f.seek(pos)
+    line = f.readline()
+    q1 = line.find('"')
+    q2 = line.find('"', q1 + 1)
+    return pos + q1 + 2, pos + q2 - 1  # starts with '#'
+
+
+def get_password_pos(f: IO, pos: int):
+    f.seek(pos)
+    line = f.readline()
+    q2 = line.rfind('"')
+    q1 = line.rfind('"', 0, q2 - 1)
+    return pos + q1 + 3, pos + q2 - 1  # starts with '0x'
+
+
+def replace_by_indexes(f: IO, start: int, end: int, data: AnyStr):
+    f.seek(end)
+    tmp = f.read()
+    f.seek(start)
+    f.write(data)
+    f.write(tmp)
+    f.truncate()
 
 
 def find_by_username(f: IO, username: str):
@@ -72,3 +121,6 @@ def find_by_username(f: IO, username: str):
         if from_hex(line[q1 + 1: q2]) == username:  # begin with '#'
             yield start, end, from_hex(line[q3 + 2: q4]), q3  # begin with '0x'
         start = end
+
+
+controls: Controls = StrokeControls()
