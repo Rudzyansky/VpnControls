1054c7ad606b331e74c1b4c71d8107074450103d
Evgeny <evgeny@false.team>
2022-08-28 15:20:16 +0300

    Domain: extract _get_account method + fix other methods

--- domain/accounting.py
+++ domain/accounting.py
@@ -5,15 +5,17 @@ from controls import utils, controls
 from entities.account import Account
 
 
+def _get_account(user_id: int, id: int, position: int):
+    user_data = controls.get_account(user_id, position)
+    if user_data:
+        return Account(id, user_data[0], user_data[1])
+    else:
+        raise RuntimeError(f'Account {user_id}:{position} not found')
+
+
 def get_account(user_id: int, id: int) -> Optional[Account]:
     position = database.accounting.get_account_position(user_id, id)
-    if position:
-        user_data = controls.get_account(user_id, position)
-        if user_data:
-            return Account(id, user_data[0], user_data[1])
-        else:
-            raise RuntimeError(f'Account {user_id}:{position} not found')
-    return None
+    return _get_account(user_id, id, position) if position else None
 
 
 def create_account(user_id: int, username: str) -> Account:
@@ -26,30 +28,42 @@ def create_account(user_id: int, username: str) -> Account:
 @database.connection()
 def delete_account(user_id: int, id: int, c) -> bool:
     position = database.accounting.get_account_position(id, c)
-    diff = controls.remove_user(user_id, position)
-    if diff != 0:
-        database.accounting.move_accounts(user_id, position, diff, c)
-    database.accounting.remove_account(id, c)
-    return True
+    if position:
+        diff = controls.remove_user(user_id, position)
+        if diff:
+            if diff != 0:
+                database.accounting.move_accounts(user_id, position, diff, c)
+            database.accounting.remove_account(id, c)
+            return True
+        else:
+            raise RuntimeError(f'Out of range {user_id}:{position}')
+    return False
 
 
 @database.connection()
-def change_username(user_id: int, id: int, new_username: str, c) -> Account:
+def change_username(user_id: int, id: int, new_username: str, c) -> Optional[Account]:
     position = database.accounting.get_account_position(user_id, id, c)
-    account = controls.get_account(user_id, id)
-    account.username = new_username
-    diff = controls.set_username(user_id, position, account.username)
-    if diff != 0:
-        database.accounting.move_accounts(user_id, position, diff, c)
-    return account
+    if position:
+        diff = controls.set_username(user_id, position, new_username)
+        if diff:
+            if diff != 0:
+                database.accounting.move_accounts(user_id, position, diff, c)
+            return _get_account(user_id, id, position)
+        else:
+            raise RuntimeError(f'Out of range {user_id}:{position}')
+    return None
 
 
 @database.connection()
-def reset_password(user_id: int, id: int, c) -> Account:
+def reset_password(user_id: int, id: int, c) -> Optional[Account]:
     position = database.accounting.get_account_position(user_id, id, c)
-    account = controls.get_account(user_id, id)
-    account.password = utils.gen_password()
-    diff = controls.set_password(user_id, position, account.password)
-    if diff != 0:
-        database.accounting.move_accounts(user_id, position, diff, c)
-    return account
+    if position:
+        password = utils.gen_password()
+        diff = controls.set_password(user_id, position, password)
+        if diff:
+            if diff != 0:
+                database.accounting.move_accounts(user_id, position, diff, c)
+            return _get_account(user_id, id, position)
+        else:
+            raise RuntimeError(f'Out of range {user_id}:{position}')
+    return None
