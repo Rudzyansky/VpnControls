2755f4c3e66753c53349bf9ec0e9c9b336c08f2c
Evgeny <evgeny@false.team>
2022-09-01 20:04:37 +0300

    domain: types: CacheList implementation

---
+++ domain/types/CacheList.py
@@ -0,0 +1,37 @@
+from typing import Generic, TypeVar
+
+from domain.types.Cache import Cache
+
+_KT = TypeVar('_KT')
+_VT = TypeVar('_VT')
+
+
+class CacheList(Cache[_KT, list[_VT]], Generic[_KT, _VT]):
+    def __setitem__(self, key: _KT, values: list[_VT]):
+        data = self._data.get(key)
+        if data is None:
+            data = list[_VT]()
+            self._data[key] = data
+        else:
+            data.clear()
+        data.extend(values)
+
+    def __getitem__(self, key: _KT) -> list[_VT]:
+        data = self._data.get(key)
+        if data is None:
+            data = list[_VT]()
+            self._data[key] = data
+        return data
+
+    def add(self, key: _KT, values: list[_VT]):
+        if key in self._data.keys():
+            self._data[key].extend(values)
+        else:
+            self._data[key] = values.copy()
+
+    def remove(self, key: _KT, values: list[_VT]):
+        if key in self._data.keys():
+            data = self._data[key]
+            for value in values:
+                if value in data:
+                    data.remove(value)
