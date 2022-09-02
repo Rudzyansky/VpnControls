6624ef43115e9265d3b0dc76663b311715e6e692
Evgeny <evgeny@false.team>
2022-09-02 13:16:42 +0300

    database: registration: fix methods: add_user; use_token; get_token; get_all_tokens; get_actual_tokens; get_next_actual_token.

--- database/sqlite/registration.py
+++ database/sqlite/registration.py
@@ -15,7 +15,7 @@ class RegistrationSqlite(Registration):
     @classmethod
     def add_user(cls, user: User, c: ConnectionSqlite = None) -> bool:
         sql = 'INSERT INTO users (id, tokens_limit, accounts_limit, owner_id, language, commands) ' \
-              'SELECT ?, t.owner_id, ?, ?, ?, ? ' \
+              'SELECT ?, ?, ?, t.owner_id, ?, ? ' \
               'FROM tokens t INNER JOIN users u on u.id = t.owner_id ' \
               'WHERE t.used_by = ?'
         return c.update_one(sql, user.id, user.tokens_limit, user.accounts_limit,
@@ -40,8 +40,10 @@ class RegistrationSqlite(Registration):
     @classmethod
     def use_token(cls, token: bytes, owner_id: int, user_id: int, c: ConnectionSqlite = None) -> bool:
         sql = 'UPDATE tokens SET used_by = ? ' \
-              'WHERE token = ? AND owner_id = ? AND used_by IS NULL AND CURRENT_DATE < expire'
-        return c.update_one(sql, user_id, token, owner_id)
+              'WHERE token = ? AND owner_id = ? ' \
+              'AND (used_by IS NULL OR used_by = ?) ' \
+              'AND CURRENT_DATE < expire'
+        return c.update_one(sql, user_id, token, owner_id, user_id)
 
     @classmethod
     def add_token(cls, token: bytes, owner_id: int, c: ConnectionSqlite = None) -> bool:
@@ -58,19 +60,19 @@ class RegistrationSqlite(Registration):
         if result is None:
             return None
         token, expire, used_by = result
-        return Token(data=token, expire=expire, used_by=used_by)
+        return Token(data=token, expire=expire, used_by=used_by, owner_id=owner_id)
 
     @classmethod
     def get_all_tokens(cls, owner_id: int, c: ConnectionSqlite = None) -> list[Token]:
         sql = 'SELECT token, expire, used_by FROM tokens WHERE owner_id = ? AND CURRENT_DATE < expire'
-        return [Token(data=token, expire=expire, used_by=used_by)
+        return [Token(data=token, expire=expire, used_by=used_by, owner_id=owner_id)
                 for token, expire, used_by in c.fetch_all(sql, owner_id)]
 
     @classmethod
     def get_actual_tokens(cls, owner_id: int, c: ConnectionSqlite = None) -> list[Token]:
         sql = 'SELECT token, expire, used_by FROM tokens WHERE owner_id = ? ' \
               'AND (used_by IS NULL OR used_by != owner_id) AND CURRENT_DATE < expire'
-        return [Token(data=token, expire=expire, used_by=used_by)
+        return [Token(data=token, expire=expire, used_by=used_by, owner_id=owner_id)
                 for token, expire, used_by in c.fetch_all(sql, owner_id)]
 
     @classmethod
@@ -88,7 +90,7 @@ class RegistrationSqlite(Registration):
               'AND (used_by IS NULL OR used_by != owner_id) AND CURRENT_DATE < expire LIMIT ?, 1'
         result = c.fetch_one(sql, owner_id, offset)
         token, expire, used_by = result
-        return Token(data=token, expire=expire, used_by=used_by)
+        return Token(data=token, expire=expire, used_by=used_by, owner_id=owner_id)
 
     @classmethod
     def count_of_actual_tokens(cls, owner_id: int, c: ConnectionSqlite = None) -> int:
